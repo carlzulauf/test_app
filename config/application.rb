3c421b035bd032b91ff60e3d74b957651c37c7fa
Carl Zulauf <czulauf@lyconic.com>
2012-02-28 01:53:11 -0600

    A more reasonable way to overcome the has_many :through counter bug

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,24 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+class ActiveRecord::Associations::HasManyThroughAssociation
+  def insert_record(record, validate = true, raise = false)
+    ensure_not_nested
+
+    if record.new_record?
+      if raise
+        record.save!(:validate => validate)
+      else
+        return unless record.save(:validate => validate)
+      end
+    end
+
+    save_through_record(record)
+    #update_counter(1)
+    record
+  end
+end
+
 module TestApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
